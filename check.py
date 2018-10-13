4485fc8c56caff10278fc1e25b063cab3a08c296
tzing <tzingshih@gmail.com>
2018-10-14 00:41:20 +0800

    rescan for duplicated questions

--- check.py
+++ check.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Check if there exists the duplicated question in the true and false question.
 """
 import argparse
 import re
+import unicodedata
 
 INDENT_SPACE = 4
 TAB_SPACE = 6
+
+CHINESE_DIGITS = "零一二三四五六七八九"
+CHINESE_UNITS = "十百千"
 
 
 def main():
@@ -34,14 +38,21 @@ def main():
         # retrieve question and answer
         ans = match.group('ans').upper()
 
+        # unified symbols
         question = match.group('question')
         question = re.sub('\s', '', question)
-        question = question.replace(',', '，')
-        question = question.replace(':', '：')
-        question = question.replace(';', '；')
-        question = question.replace('(', '（')
-        question = question.replace(')', '）')
-        question = question.replace('?', '？')
+        question = unicodedata.normalize('NFKC', question)
+
+        # unified number
+        for m in reversed(list(re.finditer(r'\d+', question))):
+            number = integer2chinese(int(m.group()))
+            question = question[:m.start()] + number + question[m.end():]
+
+        # unified text
+        question = question.replace('替代役役男', '替代役男')
+        question = question.replace('半年', '六個月')
+        question = question.replace('兩', '二')
+        question = re.sub(r'。$', '', question)
 
         questions.append((n_line, ans, question))
 
@@ -73,6 +84,41 @@ def main():
                 msg = msg.expandtabs(TAB_SPACE)
                 print(indent * 2 + msg)
         exit(1)
+
+
+def integer2chinese(num):
+    """Convert integers into chinese representation.
+    """
+    assert isinstance(num, int)
+
+    # number to chinese
+    num_name = ""
+    level = 0
+    while num > 0:
+        num_name = get_chinese_number_name(level, num % 10) + num_name
+        num = num // 10
+        level += 1
+
+    # remove zeros
+    num_name = re.sub(r'零+$', '', num_name)  # tailing zeros
+    num_name = re.sub(r'零+', '零', num_name)  # continue zeros
+
+    return num_name
+
+
+def get_chinese_number_name(level, number):
+    # number name
+    if number == 0:
+        return CHINESE_DIGITS[0]
+    name = CHINESE_DIGITS[number]
+
+    # level unit
+    if level == 1 and number == 1:
+        return CHINESE_UNITS[0]
+    elif level > 0:
+        name += CHINESE_UNITS[level % 4 - 1]
+
+    return name
 
 
 if __name__ == '__main__':
