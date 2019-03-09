fafe84065a460dbfc868809da2490647cf32ce65
Yi Hsiao <hsiaoyi0504@gmail.com>
2019-03-10 02:03:01 +0800

    add script for generating pdf files

---
+++ generate.sh
@@ -0,0 +1,15 @@
+mkdir pdf
+mdpdf law/替代役役男權利實施辦法.md pdf/替代役役男權利實施辦法.pdf
+mdpdf law/替代役實施條例.md pdf/替代役實施條例.pdf
+mdpdf law/替代役役男輔導教育辦法.md pdf/替代役役男輔導教育辦法.pdf
+mdpdf law/替代役役男獎懲辦法.md pdf/替代役役男獎懲辦法.pdf
+mdpdf law/一般替代役役男訓練服勤管理辦法.md pdf/一般替代役役男訓練服勤管理辦法.pdf
+mdpdf law/替代役役男請假規則.md pdf/替代役役男請假規則.pdf
+mdpdf law/停役替代役役男免予回役作業規定.md pdf/停役替代役役男免予回役作業規定.pdf
+mdpdf law/替代役實施條例施行細則.md pdf/替代役實施條例施行細則.pdf
+mdpdf law/替代役役男就醫醫療費用補助作業規定.md pdf/替代役役男就醫醫療費用補助作業規定.pdf
+mdpdf law/替代役役男保險實施辦法.md pdf/替代役役男保險實施辦法.pdf
+mdpdf law/內政部役政署替代役基礎訓練成績評比規定.md pdf/內政部役政署替代役基礎訓練成績評比規定.pdf
+mdpdf law/替代役役男撫卹實施辦法.md pdf/替代役役男撫卹實施辦法.pdf
+mdpdf multiple_choice.md pdf/multiple_choice.pdf
+mdpdf true_and_false.md pdf/true_and_false.pdf
